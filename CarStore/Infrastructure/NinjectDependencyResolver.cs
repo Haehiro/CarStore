5f1a863a0f750d772d7e9a80fe43739753b0ff28
Роман Ильинский <hacverr@gmail.com>
2019-04-11 18:17:41 +0300

    first commit

---
+++ CarStore/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarStore.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelparam)
+        {
+            kernel = kernelparam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<ICarRepository> mock = new Mock<ICarRepository>();
+            mock.Setup(m => m.Cars).Returns(new List<Car>{
+                new Car {Mark = "BMW", Model = "M3", Price = 200000 },
+                new Car { Mark = "Hundai", Model = "Solaris", Price = 500000 },
+                new Car { Mark = "Vaz", Model = "2109", Price = 100000 }
+            });
+
+            kernel.Bind<ICarRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
